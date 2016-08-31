77fa09504781156e93ef7b5ea8f7267f59594107
Nathan <radmaps@yahoo.com>
2016-08-31 14:07:15 -0500

    Update sparsify.js

--- sparsify.js
+++ sparsify.js
@@ -19,12 +19,14 @@
              ll = l < lp ? l:lp;  // obtain the number of columns that can be compared
              for(var i = 0; i < ll; i++) {
                prevCell = myPrevCols[i];
-               prevCellText = prevCell.childNodes[0];
                myCell = myCols[i];
-               myCellText = myCell.childNodes[0];
-               if (myCellText && prevCellText && (myCellText.data == prevCellText.data)) { // test params before using
-                  if (classToRemove && (classToRemove.length)) myCell.classList.remove(classToRemove);
-                  if (classToAdd && (classToAdd.length)) myCell.classList.add(classToAdd);
+               if (myCell && prevCell) {
+                 prevCellText = prevCell.childNodes[0];
+                 myCellText = myCell.childNodes[0];
+                 if (myCellText && prevCellText && (myCellText.data == prevCellText.data)) { // test params before using
+                    if (classToRemove && (classToRemove.length)) myCell.classList.remove(classToRemove);
+                    if (classToAdd && (classToAdd.length)) myCell.classList.add(classToAdd);
+                 }
                }
              } // for i
           } // if myCols
